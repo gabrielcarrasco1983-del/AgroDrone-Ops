ec1bd2499fe4d7a187f9fa0eb2d8d5f4be5a775a
gabrielcarrasco1983-del <gabriel.carrasco1983@gmail.com>
2025-12-26 19:35:03 -0300

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -4,4 +4,11 @@ echo "\
 enableCORS = false\n\
 headless = true\n\
 port = $PORT\n\
+\n\
+[theme]\n\
+primaryColor = '#88D600'\n\
+backgroundColor = '#FFFFFF'\n\
+secondaryBackgroundColor = '#F2F2F2'\n\
+textColor = '#000000'\n\
+base = 'light'\n\
 " > ~/.streamlit/config.toml
